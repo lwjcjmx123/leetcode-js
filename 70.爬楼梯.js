4edf4eba5bceaec76320ba8cecc71f5cba760b7e
liuwenjun <lwjcjmx123@gmail.com>
2019-05-15 11:44:52 +0800

    添加动态规划解法

--- 70.爬楼梯.js
+++ 70.爬楼梯.js
@@ -8,23 +8,40 @@
  * @return {number}
  */
 // 建一个map 缓存计算结果，避免重复运算，降低复杂度。
-let m = new Map()
+// let m = new Map()
+// // 斐波那契数列解法
+// var climbStairs = function(n) {
+//     if(n === 1) {
+//         return 1
+//     }
+//     if(n === 2) {
+//         return 2
+//     }
+    
+//     if(m.get(n)){
+//         return m.get(n)
+//     }
+//     else {
+//         let value = climbStairs(n - 1) + climbStairs(n - 2)
+//         m.set(n, value)
+//         return value
+//     }
+// };
 
-var climbStairs = function(n) {
-    if(n === 1) {
+// 动态规划
+var climbStairs = function (n) {
+    if (n === 1) {
         return 1
     }
-    if(n === 2) {
+    if (n === 2) {
         return 2
     }
-    
-    if(m.get(n)){
-        return m.get(n)
-    }
-    else {
-        let value = climbStairs(n - 1) + climbStairs(n - 2)
-        m.set(n, value)
-        return value
-    }
-};
 
+    let a = 1, b = 2, temp = 0;
+    for(let i = 3; i<=n; i++) {
+        temp = a + b
+        a = b
+        b = temp
+    }
+    return temp
+};
