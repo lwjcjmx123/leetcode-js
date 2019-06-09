92ee0992d204daa1a95e76b6a7dc7ba49d01a34c
liuwenjun <lwjcjmx123@gmail.com>
2019-06-09 17:06:18 +0800

    数学规律

---
+++ 292.nim-游戏.js
@@ -0,0 +1,13 @@
+/*
+ * @lc app=leetcode.cn id=292 lang=javascript
+ *
+ * [292] Nim 游戏
+ */
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var canWinNim = function(n) {
+    return n%4
+};
+
